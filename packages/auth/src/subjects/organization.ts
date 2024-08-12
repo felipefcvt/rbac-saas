5626fff92dfeaee3a3fc2d61476a8062db2d49f0
felipeferreira <felipefcvto@gmail.com>
2024-08-12 00:43:30 -0300

    definindo permissions do app

--- packages/auth/src/subjects/organization.ts
+++ packages/auth/src/subjects/organization.ts
@@ -3,7 +3,6 @@ import { organizationSchema } from '../models/organization'
 
 export const organizationSubject = z.tuple([
   z.union([
-    z.literal('create'),
     z.literal('delete'),
     z.literal('manage'),
     z.literal('update'),
