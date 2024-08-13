6097aa0b029e14aa58ed902ac5bb6368d354f3bb
felipeferreira <felipefcvto@gmail.com>
2024-08-13 01:07:28 -0300

    rota cadastro de user

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { prisma } from '../../../lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +16,28 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User created!'
-    },
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: { email}
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({ message: 'Email with same email already exists.'})
+      }
+
+      const passwordHash = await hash(password, 5)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash
+        }
+      })
+
+      return reply.status(201).send()
+    }
   )
 }
