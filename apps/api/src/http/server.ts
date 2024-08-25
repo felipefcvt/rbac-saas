eff2d05a3e146fac89ffea2474c25b75cdf8c63e
felipeferreira <felipefcvto@gmail.com>
2024-08-25 03:28:45 -0300

    remover projeto

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -25,6 +25,7 @@ import { updateOrganization } from './routes/orgs/update-organization'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { transferOrganization } from './routes/orgs/transfer-organization'
 import { createProject } from './routes/projects/create-project'
+import { deleteProject } from './routes/projects/delete-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -79,6 +80,7 @@ app.register(shutdownOrganization)
 app.register(transferOrganization)
 
 app.register(createProject)
+app.register(deleteProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
