1ade36450d836e3d033bdee62bef692e0e00f023
felipeferreira <felipefcvto@gmail.com>
2024-08-25 03:19:01 -0300

    criaçaão de projeto

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -24,6 +24,7 @@ import { getOrganization } from './routes/orgs/get-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { transferOrganization } from './routes/orgs/transfer-organization'
+import { createProject } from './routes/projects/create-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -76,6 +77,8 @@ app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
 app.register(transferOrganization)
+
+app.register(createProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
