d73538b2ef483516d67a7e5a4abaf23e5423842b
felipeferreira <felipefcvto@gmail.com>
2024-08-26 14:07:50 -0300

    hook useStateAction

---
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -0,0 +1,23 @@
+'use server'
+
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
+export async function signInWithEmailAndPassword(
+  previousState: any,
+  data: FormData
+) {
+  console.log(previousState)
+
+  const { email, password } = Object.fromEntries(data)
+
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const result = await signInWithPassword({
+    email: String(email),
+    password: String(password),
+  })
+
+  console.log(result)
+
+  return 'Success'
+}
